ead4fa749069f975b3eb0e157049518749d2d18f
Abdullah Ajmal <abdullah.ajmal.7@hotmail.com>
2021-07-29 15:44:41 +0500

    Added vc to project. Updated config settings.

---
+++ src/config/config.ts
@@ -0,0 +1,18 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "postgres",
+    "host": "udagramabdullahdev.cy81gfxyq4yl.ap-southeast-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "ap-southeast-1",
+    "aws_media_bucket": "udagramabdullah-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
